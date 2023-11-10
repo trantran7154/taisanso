44c68919feab93e19879d6cbea59501ed606802e
trantran7154 <54873044+trantran7154@users.noreply.github.com>
2023-11-10 22:09:16 +0700

    Added my project

---
+++ taisanso/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <Menu />
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Menu from './components/Menu.vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Menu
+  },
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ taisanso/src/components/Menu.vue
@@ -0,0 +1,154 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-title> <v-img max-height="50" max-width="200" src="../img/logo.jpg"></v-img></v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn text>
+                Trang chủ
+            </v-btn>
+            <v-menu transition="scale-transition" :close-on-content-click="false" :nudge-width="1500" offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn text v-bind="attrs" v-on="on">
+                        Sản phẩm
+                    </v-btn>
+                </template>
+                <v-card>
+                    <v-tabs vertical>
+                        <v-tab v-for="item in itemTab" :key="item" class="tab text-left">
+                            <v-row>
+                                <v-col>
+                                    <v-icon left class="mt-3">
+                                        {{ item.icon }}
+                                    </v-icon>
+                                </v-col>
+                                <v-col>
+                                    <div class="mt-2">
+                                        {{ item.title }}
+                                        <br />
+                                        <small mt-3 style="color: darkgray !important;"> {{ item.description }}</small>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </v-tab>
+
+                        <v-tab-item class="pa-3" v-for="items in itemTab" :key="items">
+                            <div style="color: #1976d2">Dịch vụ chính</div>
+                            <v-row>
+                                <v-col cols="3" v-for="item1 in items.subTab" :key="item1">
+                                    <div class="mt-3 ml-3">
+                                        <v-icon>{{ item1.subIcon }}</v-icon> <a>{{ item1.subTitle }}</a>
+                                        <br>
+                                        <small style="color: darkgray !important;"> {{ item1.subDescription }}</small>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </v-tab-item>
+                    </v-tabs>
+                </v-card>
+            </v-menu>
+            <v-menu transition="scale-transition" offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn text v-bind="attrs" v-on="on">
+                        Dịch vụ
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="(item, index) in itemService" :key="index">
+                        <v-list-item-title><v-icon>{{ item.icon }}</v-icon> <a>{{ item.title }}</a></v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+            <v-btn text>
+                Hỗ trợ
+            </v-btn>
+            <v-btn text>
+                Blog
+            </v-btn>
+        </v-toolbar-items>
+        | <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-icon>mdi-phone</v-icon> <b class="red--text ml-2">0123456789</b>
+    </v-toolbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: 'Home',
+
+    components: {
+    },
+
+    data() {
+        return {
+            menu: false,
+            selectedItem: 1,
+            itemTab: [
+                {
+                    title: 'Email', icon: 'mdi-email', description: 'Kinh doanh với Email',
+                    subTab: [
+                        { subTitle: 'Gmail USA', subDescription: 'Kho 4000+ mail.', subIcon: 'mdi-email-fast-outline' },
+                        { subTitle: 'Hotmail', subDescription: 'Kho 4000+ mail.', subIcon: 'mdi-email-fast-outline' },
+                        { subTitle: 'Gmail', subDescription: 'Kho 4000+ mail.', subIcon: 'mdi-email-fast-outline' },
+                        { subTitle: 'Email theo Domain', subDescription: 'Kho 4000+ mail.', subIcon: 'mdi-email-fast-outline' },
+                    ]
+                },
+                {
+                    title: 'Phần mềm', icon: 'mdi-code-braces', description: 'Quản lý kinh doanh, doanh nghiệp',
+                    subTab: [
+                        { subTitle: 'Netflix', subDescription: 'Cung cấp tài khoản vip.', subIcon: 'mdi mdi-netflix' },
+                        { subTitle: 'Tools', subDescription: 'Chạy tool theo yêu cầu.', subIcon: 'mdi-tools' },
+                    ]
+                },
+                {
+                    title: 'Tài khoản', icon: 'mdi-account', description: 'Mua bán tài khoản',
+                    subTab: [
+                        {
+                            subTitle: 'Facebook', subDescription: 'Cung cấp tài khoản facebook ảo.', subIcon: 'mdi-facebook'
+                        },
+                        { subTitle: 'Google', subDescription: 'Cung cấp tài khoản google ảo.', subIcon: 'mdi-google' },
+                    ]
+                },
+                {
+                    title: 'Internet', icon: 'mdi-earth', description: 'Hạ tầng lưu trữ, điện toán',
+                    subTab: [
+                        { subTitle: 'Hosting', subDescription: 'Cung cấp hosting dành riêng cho doanh nghiệp, giáo dục...', subIcon: 'mdi-cloud-check-outline' },
+                        { subTitle: 'Domain', subDescription: 'Kiểm tra tên miền miễn phí, nhanh chóng chỉ với 3 giây có ngay domain chất lượng.', subIcon: 'mdi mdi-web' },
+                        { subTitle: 'Vps', subDescription: 'Gói giải pháp hạ tầng dành riêng cho phát triển web, ứng dụng trên windows, linux.', subIcon: 'mdi-server-minus-outline' },
+                    ]
+                },
+            ],
+            itemService: [
+                { title: 'Làm phần mềm', icon:'mdi-xml' },
+                { title: 'Blockchain', icon:'mdi-hand-coin-outline' },
+                { title: 'Tăng tương tác', icon:'mdi-arrow-up' },
+            ]
+        }
+    }
+})
+</script>
+
+<style scoped>
+.menu {
+    border-radius: 15px !important;
+    background-color: #E0F7FA;
+    width: 300px;
+}
+
+.menu .v-list-item:hover {
+    background-color: #FFFFFF !important;
+    border-radius: 10px !important;
+}
+
+.tab {
+    text-transform: none;
+    margin-right: auto;
+    display: inline-block;
+}
+
+.v-tabs--vertical>.v-tabs-bar .v-tab {
+    height: 54px;
+}
+</style>
