922c26729b44e4791531955dc9a867102abc614f
trantran7154 <vuongbaot1905@gmail.com>
2023-12-13 12:41:44 +0700

    p

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -1,43 +1,23 @@
 <template>
   <div class="d-flex align-center justify-center login" style="font-size: 14px">
     <v-sheet width="300" class="mx-auto">
-      <v-form
-        ref="formLogin"
-        v-model="formLogin.valid"
-        lazy-validation
-        fast-fail
-      >
-        <v-img
-          v-if="size.onsize <= 860"
-          width="200"
-          height="70"
-          style="margin-left: -31px"
-          src="../../img/logo.jpg"
-        ></v-img>
+      <v-form ref="formLogin" v-model="formLogin.valid" lazy-validation fast-fail>
+        <v-img v-if="size.onsize <= 860" width="200" height="70" style="margin-left: -31px"
+          src="../../img/logo.jpg"></v-img>
+
         <h3 style="font-size: 24px">Đăng nhập</h3>
 
-        <v-alert
-          v-if="message"
-          class="mt-5"
-          style="border: 1px solid red; font-size: 12px"
-          border="right"
-          colored-border
-          type="error"
-          elevation="0"
-        >
+        <p>Chào mừng bạn đến với taisanso!</p>
+
+        <v-alert v-if="message" class="mt-5" style="border: 1px solid red; font-size: 12px" border="right" colored-border
+          type="error" elevation="0">
           {{ message }}
         </v-alert>
 
         <div class="mt-5">
           <label style="color: #6d7a82" class="font-weight-bold">Email</label>
-          <v-text-field
-            class="mt-2"
-            placeholder="exampe@gmail.com"
-            v-model="formLogin.value.email"
-            :rules="formLogin.validate.email"
-            persistent-hint
-            outlined
-          ></v-text-field>
+          <v-text-field class="mt-2" placeholder="exampe@gmail.com" v-model="formLogin.value.email"
+            :rules="formLogin.validate.email" persistent-hint outlined></v-text-field>
 
           <div style="color: #6d7a82">
             <!-- <router-link
@@ -47,16 +27,8 @@
             > -->
           </div>
 
-          <v-btn
-            class="white--text btn-login mb-5 mt-5"
-            color="var(--color-5)"
-            block
-            depressed
-            large
-            @click="login()"
-            :loading="isLoading"
-            >Đăng nhập</v-btn
-          >
+          <v-btn class="white--text btn-login mb-5 mt-3" color="var(--color-5)" block depressed large @click="login()"
+            :loading="isLoading">Đăng nhập</v-btn>
 
           <!-- <div class="mt-5" style="color: #6d7a82">
             Bạn chưa có tài khoản?
@@ -67,14 +39,12 @@
 
           <div class="mt-10 d-flex">
             <v-divider></v-divider>
-            <p class="pa-4">hoặc là</p>
+            <span class="pa-4 or">hoặc là</span>
             <v-divider></v-divider>
           </div>
 
           <v-btn block outlined x-large class="text-none" disabled>
-            <img
-              src="https://assets.hostinger.com/images/login/google-213cd1c446.svg"
-            />
+            <img src="https://assets.hostinger.com/images/login/google-213cd1c446.svg" />
 
             <span class="ml-2">Login google</span>
           </v-btn>
@@ -90,7 +60,7 @@ import authApi from "@/api/auth.api";
 
 export default Vue.extend({
   name: "Footer",
-  created() {},
+  created() { },
   props: ["size", "user"],
   data() {
     return {
@@ -139,6 +109,36 @@ export default Vue.extend({
 .login {
   font-family: Muli, sans-serif;
   padding: 50px 0px 50px 0px;
+  width: 380px;
+  height: 500px;
+  border: 1px solid #CCC;
+  margin: 30px auto;
+  border-radius: 20px;
+
+  p {
+    white-space: nowrap;
+    overflow: hidden;
+    font-family: 'Source Code Pro', monospace;
+    font-size: 17px;
+    color: #979797;
+  }
+
+  /* Animation */
+  p {
+    animation: animated-text 2s steps(30, end) 0s 1 normal both;
+  }
+
+  /* text animation */
+
+  @keyframes animated-text {
+    from {
+      width: 0;
+    }
+
+    to {
+      width: 100%;
+    }
+  }
 
   .btn {
     align-items: center !important;
@@ -151,7 +151,7 @@ export default Vue.extend({
     padding: 12px 24px;
   }
 
-  p {
+  .or {
     margin-top: -29px;
     color: #979797;
     font-size: 17px;
@@ -161,12 +161,6 @@ export default Vue.extend({
     font-size: 14px;
     text-transform: none;
     font-weight: bold;
-  }
-
-  .forgot {
-    text-decoration: none;
-    color: black;
-    font-size: 16px;
   }
 }
 </style>
